207476b9d6e27a360cd2a06db472d3811bef6746
Stratos Lontzetidis <s.lontzetidis@gmail.com>
2020-10-04 18:57:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+response = requests.get("https://api.xforce.ibmcloud.com/xfti/mw/ipv4", auth = HTTPBasicAuth('1f783594-dbd8-4884-9fd0-2c6fcc633415','83866f36-ffe7-4e5f-b931-e34307d83f82'))
+print(json.dumps(response.json(), indent=4, sort_keys=True))
