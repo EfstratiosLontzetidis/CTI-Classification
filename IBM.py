49dd9bed6d163dc8844738ff6a06dee6c323a70c
Stratos Lontzetidis <s.lontzetidis@gmail.com>
2020-10-09 18:55:20 +0300

    Update and rename ibm.py to IBM.py

---
+++ IBM.py
@@ -0,0 +1,16 @@
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+
+class IBM:
+
+    def __init__(self):
+        self.data = []
+
+    # IBM API connect
+    def ibm_api(self):
+        response = requests.get("https://api.xforce.ibmcloud.com/xfti/mw/ipv4",
+                                auth=HTTPBasicAuth('1f783594-dbd8-4884-9fd0-2c6fcc633415',
+                                                   '83866f36-ffe7-4e5f-b931-e34307d83f82'))
+        return json.dumps(response.json(), indent=4, sort_keys=True)
