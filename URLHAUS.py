7bbf44161ac6c40e45d2fece7cf42d3ed4bdeefc
Stratos Lontzetidis <s.lontzetidis@gmail.com>
2020-10-09 18:57:18 +0300

    Update and rename urlhaus.py to URLHAUS.py

---
+++ URLHAUS.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+
+class URLHAUS:
+
+    def __init__(self):
+        self.data = []
+
+
+    # URLhaus API connect
+    def urlhaus_api(self):
+        response = requests.get("https://urlhaus-api.abuse.ch/v1/payloads/recent/")
+        return json.dumps(response.json(), indent=4, sort_keys=True)
