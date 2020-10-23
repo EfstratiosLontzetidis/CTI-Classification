ed3a130489766c00c629c1ebd72ab2e49ee01771
Stratos Lontzetidis <s.lontzetidis@gmail.com>
2020-10-23 13:22:20 +0300

    Imported otx,urlhaus,misp classes

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from CTIServerConnector.SuperConnector import SuperConnector
+from CTIServerConnector.MISP import MISP
+from CTIServerConnector.OTX import OTX
 from CTIServerConnector.IBM import IBM
+from CTIServerConnector.URLHAUS import URLHAUS
 
 
 
 connector = SuperConnector()
-connection_behavior = IBM()
+connection_behavior = MISP()
 connector.setConnectorBehaviour(connection_behavior)
 
 ctips = connector.getCTIPs()
