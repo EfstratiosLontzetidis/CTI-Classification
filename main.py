1bc0c8457c6768545d55bbea8e83fb4e0a4fdb92
Stratos Lontzetidis <s.lontzetidis@gmail.com>
2020-10-26 14:55:08 +0200

    added stix2validator, misp class is functioning

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from CTIServerConnector.SuperConnector import SuperConnector
 from CTIServerConnector.MISP import MISP
 from CTIServerConnector.OTX import OTX
+from CTIServerConnector.SuperConnector import SuperConnector
 from CTIServerConnector.IBM import IBM
 from CTIServerConnector.URLHAUS import URLHAUS
-
-
+from stix2validator import validate_file, print_results
 
 connector = SuperConnector()
 connection_behavior = MISP()
@@ -13,3 +12,5 @@ connector.setConnectorBehaviour(connection_behavior)
 ctips = connector.getCTIPs()
 print(ctips)
 
+#results = validate_file("ctifeed2.json")
+#print_results(results)
