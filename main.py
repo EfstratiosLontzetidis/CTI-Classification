660a1fcc6474d22c430117bf129cb7108f73f971
Stratos Lontzetidis <s.lontzetidis@gmail.com>
2020-11-13 15:13:34 +0200

    import opencti class

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from CTIServerConnector.OTX import OTX
 from CTIServerConnector.SuperConnector import SuperConnector
 from CTIServerConnector.IBM import IBM
 from CTIServerConnector.URLHAUS import URLHAUS
+from CTIServerConnector.OPENCTI import OPENCTI
 from stix2validator import validate_file, print_results
 
 connector = SuperConnector()
-connection_behavior = MISP()
+connection_behavior = OPENCTI()
 connector.setConnectorBehaviour(connection_behavior)
 
 ctips = connector.getCTIPs()
