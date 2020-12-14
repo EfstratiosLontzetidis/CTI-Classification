25890e2a43d316e0fcc4c117f3efa196413fb58e
Stratos Lontzetidis <s.lontzetidis@gmail.com>
2020-12-14 15:14:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-from CTIServerConnector.MISP import MISP
 from CTIServerConnector.ANOMALI import ANOMALI
+from CTIServerConnector.BLUELIV import BLUELIV
+from CTIServerConnector.MRLOOQUER import MRLOOQUER
+from CTIServerConnector.OPENCTI import OPENCTI
+from CTIServerConnector.MISP import MISP
 from CTIServerConnector.OTX import OTX
+from CTIServerConnector.PULSEDIVE import PULSEDIVE
 from CTIServerConnector.SuperConnector import SuperConnector
 from CTIServerConnector.IBM import IBM
 from CTIServerConnector.URLHAUS import URLHAUS
-from CTIServerConnector.OPENCTI import OPENCTI
-from CTIServerConnector.MRLOOQUER import MRLOOQUER
-from CTIServerConnector.PULSEDIVE import PULSEDIVE
-from CTIServerConnector.BLUELIV import BLUELIV
-from stix2validator import validate_file, print_results
+
 
 connector = SuperConnector()
-connection_behavior = PULSEDIVE()
+connection_behavior = OPENCTI()
 connector.setConnectorBehaviour(connection_behavior)
 
 ctips = connector.getCTIPs()
-print(ctips)
-
-#results = validate_file("ctifeed2.json")
-#print_results(results)
