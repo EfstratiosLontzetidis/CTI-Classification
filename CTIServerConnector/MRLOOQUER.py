907d3d596cb44886412b739706fc94f0d57b061d
slont <s.lontzetidis@gmail.com>
2021-01-18 21:20:32 +0200

    Almost done with MRLOOQUER.py, need to decise what will do with field : "last_seen"

--- CTIServerConnector/MRLOOQUER.py
+++ CTIServerConnector/MRLOOQUER.py
@@ -3,6 +3,8 @@
 import requests
 import json
 from stix2 import parse, Indicator
+from Utilities.utility import stix_to_json
+from serviceDB.mongoDBService import ClientDB
 from CTIServerConnector.SuperConnector import SuperConnector
 
 
@@ -15,7 +17,32 @@ class MRLOOQUER(SuperConnector):
     def api_con(self):
         response = requests.get("https://iocfeed.mrlooquer.com/feed.json")
         #parse the converted json
-        parse=json.loads(json.dumps(response.json(), indent=4))
-        for x in range(len(parse)):
-            print(parse[x]['domain'])
-        #return json.dumps(response.json(), indent=4)
+        parsed_CTIP=json.loads(json.dumps(response.json(), indent=4))
+        for x in range(len(parsed_CTIP)):
+            pattern="[ipv4-addr:value = "+"'"+parsed_CTIP[x]['ip4']+"'"+" AND ipv6-addr:value = "+"'"+parsed_CTIP[x]['ip6']+"'" +"]"
+            indicator=Indicator(name="Malware Dual stack (IPv4 and IPv6 for domain: "+parsed_CTIP[x]['domain'],
+                                pattern=pattern,
+                                pattern_type="stix",
+                                #created or last seen must be added and its done
+                                category=parsed_CTIP[x]['category'],
+                                subcategory=parsed_CTIP[x]['subcategory'],
+                                categorytype=parsed_CTIP[x]['type'],
+                                domain=parsed_CTIP[x]['domain'],
+                                allow_custom=True,
+                                iPv4=parsed_CTIP[x]['ip4'],
+                                iPv4ASN=parsed_CTIP[x]['ip4Asn'],
+                                iPv4NumberOfCVE=parsed_CTIP[x]['ip4NumCve'],
+                                iPv4Ports=parsed_CTIP[x]['ip4Portlist'],
+                                iPv6=parsed_CTIP[x]['ip6'],
+                                iPv6Prefix=parsed_CTIP[x]['ip6Prefix'],
+                                iPv6ASN=parsed_CTIP[x]['ip6Asn'],
+                                iPv6NumberOfCVE=parsed_CTIP[x]['ip6NumCve'],
+                                iPv6Ports=parsed_CTIP[x]['ip6Portlist'],
+                                )
+            # parse the stix2 object
+            CTIP_ioc_parsed = parse(indicator, allow_custom=True)
+            # store it in database
+            Stix2Collection = ClientDB.db["CTIPsToStix2"]
+            Stix2Collection.insert_one(stix_to_json(CTIP_ioc_parsed))
+            print(indicator)
+
