e5825636977c025ed8efef9ed9b02b974744c065
slont <s.lontzetidis@gmail.com>
2021-01-18 12:28:17 +0200

    Successful parsing for MRLOOQUER.py

--- CTIServerConnector/MRLOOQUER.py
+++ CTIServerConnector/MRLOOQUER.py
@@ -2,6 +2,7 @@
 
 import requests
 import json
+from stix2 import parse, Indicator
 from CTIServerConnector.SuperConnector import SuperConnector
 
 
@@ -13,4 +14,8 @@ class MRLOOQUER(SuperConnector):
     # MRLOOQUER get CTIPs,csv ,not one by one
     def api_con(self):
         response = requests.get("https://iocfeed.mrlooquer.com/feed.json")
-        return json.dumps(response.json(), indent=4)
+        #parse the converted json
+        parse=json.loads(json.dumps(response.json(), indent=4))
+        for x in range(len(parse)):
+            print(parse[x]['domain'])
+        #return json.dumps(response.json(), indent=4)
