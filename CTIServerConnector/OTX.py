0b387db0a02284c0e501a1a4000255bed2556750
slont <s.lontzetidis@gmail.com>
2021-01-22 13:10:11 +0200

    Begin with OTX.py

--- CTIServerConnector/OTX.py
+++ CTIServerConnector/OTX.py
@@ -11,11 +11,11 @@ class OTX(SuperConnector):
 
     def __init__(self):
         super().__init__()
-    
+
     # OTX get CTIPs
     def api_con(self):
-        #can take pulses using id,so can be taken one by one using regex
-        response = requests.get("https://otx.alienvault.com/api/v1/pulses/5fe4cb300b0a9b6655a11de1/",
+        # can take pulses using id,so can be taken one by one using regex
+        response = requests.get("https://otx.alienvault.com/api/v1/pulses/activity",
                                 auth=HTTPBasicAuth('34c7431b6d78523543910d7bdc04e2126849a8b583814c263ae9ef84b9ec58ca',
                                                    ''))
         return json.dumps(response.json(), indent=4, sort_keys=True)
