cc3a39cbf2a8a87e1a5b94ad88b653f0e0c33270
slont <s.lontzetidis@gmail.com>
2021-02-01 14:58:33 +0200

    Tested some things in MISP.py, got a file to analyze it

--- CTIServerConnector/MISP.py
+++ CTIServerConnector/MISP.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 from pymisp import PyMISP
+from Utilities.utility import stix_to_json
+from serviceDB.mongoDBService import ClientDB
 from CTIServerConnector.SuperConnector import SuperConnector
+import json
 
 
 class MISP(SuperConnector):
@@ -15,5 +18,12 @@ class MISP(SuperConnector):
         misp_verifycert = False
         #run events,take uuids from each event with regex, run get_event(uuid) loop to take one by one
         misp = PyMISP(misp_url, misp_key, misp_verifycert, debug=False)
-        data = misp.events()
-        return (data)
+        orgs = misp.organisations(scope="all")
+        orgs_json = json.dumps(orgs, indent=4, sort_keys=True)
+        orgs_parsed=json.loads(orgs_json)
+        for x in range(len(orgs_parsed)):
+            events=misp.search(controller="attributes",org=orgs_parsed[x]['Organisation']['name'])
+            print((json.dumps(events, indent=4, sort_keys=True)))
+            break
+
+
